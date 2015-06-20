2a3a6412c184e896aac773a03e211c5e084561f0
Will Murphy <wmurphy@eatright.org>
2015-06-20 18:14:21 -0500

    first version posted to github and shinyapps.io

--- server.R
+++ server.R
@@ -13,6 +13,7 @@ options(RCHART_WIDTH = 600)
 
 activities <- read.csv("mets.csv")
 
+
 shinyServer(function(input, output, clientData, session) {
 
     ### reactives
@@ -23,7 +24,7 @@ shinyServer(function(input, output, clientData, session) {
     })
     
     # calculate the estimated energy needs from the reconstructed non-linear least
-    # squares model (see setup.R) using error propagation  and monte carlo simulation
+    # squares model (see setup.R) using error propagation and monte carlo simulation
     # (propagate package)
     EER_range <- reactive({
         input$goButton
@@ -44,6 +45,49 @@ shinyServer(function(input, output, clientData, session) {
                  propUpper=p$summary$`Prop.97.5%`)
             
         })
+    })
+    
+    activity_METs <- reactive({
+        # Total of metabolic equivalents from all activities entered
+        
+        # extract the activity related inputs
+        acts <- names(input)[which(names(input) %in% activities$Activity)]
+        
+        # Abort if this function gets called before the activity inputs have been initialized
+        if(length(acts)<1) return(NA)
+        
+        input_activity_values <- sapply(acts, function(x){input[[x]]})
+        
+        # (activity MET - 1) * hours = MET-hours above basal metabolic rate
+        row.names(activities) <- activities$Activity
+        sum((activities[acts, "METs"] - 1) * input_activity_values, na.rm = TRUE)       
+    })
+    
+    MET_factor <- reactive({
+        # MET inflation factor formula from IOM energy manual
+        # Accounts for indirect increases in energy expenditure from exercise
+        # MET_factor =  1.15 (EPOC) / .9 (TEF) / BEE (estimated) / 1 MET-day
+        # MET-day = 1440 minutes * 0.0175 kcal/kg/min * weight
+        
+        # estimate basal energy requirement via Harris-Benedict
+        if(input$sex=="m") BEE <- 
+                input$weight_kg * 13.397 + input$height_cm * 4.799 - 
+                input$age * 5.677 + 88.632
+        else BEE <-
+                input$weight_kg * 9.247 + input$height_cm * 3.098 - 
+                input$age * 4.33 + 447.593
+        
+        1.15 / .9 / (BEE / (1440 * 0.0175 * input$weight_kg))        
+    })
+    
+    PAL_value <- reactive({
+        # to get PAL we take the metabolic
+        # equivalents value  multiplied by
+        # the proportion of the day spent on the activity 
+        #   (hours per week of activity ) / (total hours per week)
+        # and then multiply by inflation factor and add 1.39 for basic
+        # activities of daily living
+        activity_METs() / 168 * MET_factor() + 1.39
     })
     
     ### observers
@@ -95,13 +139,29 @@ shinyServer(function(input, output, clientData, session) {
     ### renderers
     
     #text version of estimated energy range
-    output$EER <- renderText({
-        validate(need(input$goButton > 0, "Select your age, height, weight, and physical activity level to the left, then click \"Calculate Energy Needs\""),
-        need(isolate(input$age) >= 18, "At present, energy needs are available only  for adults 18 years or older"))
-
-       paste("Estimated Energy Requirement:", 
-             round(EER_range()$propLower, digits = 0),
-             " - ", round(EER_range()$propUpper, digits = 0))
+    output$EER <- renderUI({
+        validate(need(input$goButton > 0, "Your estimated energy needs will appear after clicking the \"Calculate Energy Needs\" button to the left."),
+                 need(isolate(input$age) >= 18, "At present, energy needs are available only  for adults 18 years or older"))
+        
+        div(p("Your energy needs are estimated to be between ", 
+              span(paste(prettyNum(EER_range()$propLower, big.mark=",", digits = 0),
+              "and", prettyNum(EER_range()$propUpper, big.mark=",", digits = 0)), 
+              class="emphasizeText"),
+              " Calories per day"),p("The estimated range will include the actual energy
+        needs of 95% of people, but 5% of people will have individual energy needs higher
+        or lower than the estimate."), p("You can compare your estimate with the 
+        laboratory-measured energy needs of people similar to you using the chart below."),
+            h4("Energy Needs Comparison Chart"))
+    })
+    
+    # this caption for the chart is setup to be rendered so it doesn't appear 
+    # until the chart does
+    output$chart_caption <- renderText({
+        validate(need(input$goButton > 0, ""))
+        "Your estimated energy needs range is highlighted 
+        in green, and the points on the plot represent measured energy needs of 
+        people of the same sex with similar physical activity level, age, and 
+        body mass index. Mouse over a point on the plot to view details."
     })
  
     # renders rCharts polychart of the estimated EER range compared to nearby
@@ -109,33 +169,79 @@ shinyServer(function(input, output, clientData, session) {
     output$chart <- renderChart2({
         validate(need(input$goButton > 0, ""),
                  need(isolate(input$age) >= 18, ""))
+        pal_current <- isolate(PAL_value())
+        # if the user didn't use the PAL help panel, set the pal value to equal
+        # the average for the selected PAL category (averages from IOM report)
+        if(pal_current == 1.39) pal_current <- 
+            switch(isolate(input$PALcat), Sedentary = 1.39,
+                   Low_Active = 1.5, Active = 1.75, Very_Active = 2.2)
+        
+        # filter out values with large differences in bmi, age, and PAL
         pData <- isolate({
-            adults %>% filter(sex == input$sex, PALcat == input$PALcat) %>%
-                mutate(ageDiff = age - input$age, bmiDiff = BMI - bmi()) %>% zoomPlot 
+            adults %>% filter(sex == input$sex) %>%
+                mutate(ageDiff = age - input$age, bmiDiff = BMI - bmi(),
+                       palDiff = PAL - pal_current) %>% zoomPlot 
         })
-        p <- rPlot(TEE ~ BMI, data=pData, type="point", color=list(var="ageDiff"),
+        
+        # plot the prediction interval for the user with a wide vertica line
+#         p <- rPlot(TEE ~ BMI, type="line", size=list(const="20"),
+#                    color=list(const="#00CC66"), 
+#                    data=data.frame(BMI = c(isolate(bmi()), isolate(bmi())),
+#                                    TEE = c(EER_range()$propLower,
+#                                            EER_range()$propUpper)))
+        # ploting the prediction interval
+        # used a wide line at first, but polycharts cannot do tooltips for line charts
+        # instead, we use a sequence of overlapping points 
+        EER_range_df <- data.frame(BMI = isolate(bmi()), 
+                                   TEE = seq(EER_range()$propLower,
+                                             EER_range()$propUpper,
+                                             length.out = 50))
+        # because we use oversized circles to make the shape, the ends extend
+        # past the actual limits of the range by an amount equal to the radius
+        # we compensate by censoring the extreme points so that the outter edge
+        # of the circle from an inner point will approximate the true border
+        # of the range
+        EER_range_df <- EER_range_df[2:(nrow(EER_range_df)-2), ]
+        # With this points setup, the auto tooltip would display the erroneous values
+        # for each point. We override that with a tooltip function the returns the
+        # range limits no matter which point is highlighted
+        tt <- paste0("#! function(x) {return('You. Age: ", isolate(input$age),
+                     " BMI: ", isolate(bmi()), " PAL: ", round(pal_current, digits=2),
+                     " Energy needs: ", round(EER_range()$propLower, digits = 0),
+                     " - ", round(EER_range()$propUpper, digits = 0)," Calories')} !#")
+        p <- rPlot(TEE ~ BMI, data = EER_range_df, type="point", size=list(const=15),
+                   color=list(const="#00CC66"), tooltip = tt, opacity=list(const=100))
+        # add hidden points above and below the estimated range to keep it from
+        # touching the edge of the chart
+        p$layer(TEE ~ BMI, data=data.frame(BMI=rep(isolate(bmi()),2),
+                                                   TEE= c(EER_range()$propLower - 100,
+                                                          EER_range()$propUpper + 100)),
+                type="point", opacity=list(const=0.00001), color=list(cost="white"), 
+                tooltip= "#! function(x) {return(NULL)} !#")
+        # plot the source data as points
+        p$layer(TEE ~ BMI, data=pData, type="point", size=list(var="PAL"),
+                   color=list(var="age"),
                    tooltip = "#! function(x) { 
                                     return('Age: ' + Math.round(x.age) +
                                           ' BMI: ' + Math.round(x.BMI) + 
+                                          ' PAL: ' + Math.round(x.PAL * 100) / 100 +
                                           ' Energy needs: ' + Math.round(x.TEE) +
-                                          ' calories'); 
+                                          ' Calories'); 
                                 } !#" )
-        p$layer(x="BMI", y="TEE", type="line", size=list(const="20"),
-                opacity=list(const=".5"), color=list(const="green"), 
-                data=data.frame(BMI = c(isolate(bmi()), isolate(bmi())),
-                                TEE = c(EER_range()$propLower,
-                                        EER_range()$propUpper))
-                 )
-        p$guides(color = list(title = "Age Difference"),
+
+        p$guides(color = list(title = "Age"),
                  x = list(title = "Body Mass Index (kg/m²)"),
-                 y = list(title = "Energy Needs (Calories)"))
+                 y = list(title = "Energy Needs (Calories)"),
+                 size = list(title = "Physical Activity Level"))
         p
     }) 
-    
+
     # dynamically creates a numeric input for each activity in the activities table
     # for the PAL helper panel
     output$activityList <- renderUI({
-        act <- split(as.character(activities$Activity), cut2(1:nrow(activities), g=3))
+        act <- split(as.character(activities[order(activities$Sort, activities$Activity), 
+                                             "Activity"]), 
+                     cut2(1:nrow(activities), g=3))
         div(class = "sidebyside", 
             fluidRow(column(4,lapply(as.character(act[[1]]), 
                            function(x) {numericInput(x,x, value = 0, step=0.5, min = 0)})),
@@ -151,61 +257,29 @@ shinyServer(function(input, output, clientData, session) {
     # Writes description of physical activity level calculation to the bottom of the
     # PAL help panel, also updates the PAL_cat radio buttons based on the results
     output$PAL_calc <- renderUI({
-        
-        # MET inflation factor formula from IOM energy manual
-        # Accounts for indirect increases in energy expenditure from exercise
-        # MET_factor =  1.15 (EPOC) / .9 (TEF) / BEE (estimated) / 1 MET-day
-        # MET-day = 1440 minutes * 0.0175 kcal/kg/min * weight
-        
-        # estimate basal energy requirement via Harris-Benedict
-        if(input$sex=="m") BEE <- 
-                input$weight_kg * 13.397 + input$height_cm * 4.799 - input$age * 5.677 + 88.632
-        else BEE <-
-                input$weight_kg * 9.247 + input$height_cm * 3.098 - input$age * 4.33 + 447.593
-        
-        MET_factor <- 1.15 / .9 / (BEE / (1440 * 0.0175 * input$weight_kg))
-        
-        
-        # Total of metabolic equivalents from all activities entered
-        
-        # extract the activity related inputs
-        acts <- names(input)[which(names(input) %in% activities$Activity)]
-
-        # Abort if this function gets called before the activity inputs have been initialized
-        if(length(acts)<1) return("")
-
-        input_activity_values <- sapply(acts, function(x){input[[x]]})
-        
-        # to get relative increase in metabolism per activity we take the metabolic
-        # equivalents value (METs from the activities table) less one multiplied by
-        # the proportion of the day spent on the activity 
-        #   (hours per week of activity ) / (total hours per week)
-        # and then multiply by inflation factor
-        row.names(activities) <- activities$Activity
-        activity_METs <- 
-            sum((activities[acts, "METs"] - 1) * input_activity_values, na.rm = TRUE)
-        delta_PAL <- activity_METs / 168 * MET_factor
-        
-        # Convert calculated PAL into factor using switch and cut2 for convenience
-        # (but cut2 only works right if at least one value for each category is present
-        #  so some filler data is added and the dropped)
-        PAL_category <- switch(as.numeric(first(cut2(c(delta_PAL + 1.39, 1, 1.5, 1.7, 2), 
+        validate(need(!is.na(activity_METs()), ""))
+        # Convert calculated PAL into factor using switch and cut2 for 
+        # convenience (but cut2 only works right if at least one value 
+        # for each category is present so some filler data is added and 
+        # then dropped)
+        PAL_category <- switch(as.numeric(first(cut2(c(PAL_value(), 1, 1.5, 1.7, 2), 
                                                      cuts=c(1.4, 1.6, 1.9)))),
                                "Sedentary", "Low_Active", "Active", "Very_Active")
         
+        # update the PAL input with the resutls of the calculation
         updateRadioButtons(session, "PALcat", selected=PAL_category)                
         
-        div(p(paste("Physical activity level:", round(activity_METs, digits=1), 
+        # describe the calculation
+        div(p(paste("Physical activity:", round(activity_METs(), digits=1), 
                     "activity MET-hours per week /", 
-                    "168 hours per week ×", round(MET_factor, digits = 2), 
-                    "(personalized energy expenditure inflation factor)",
+                    "168 hours per week ×", round(MET_factor(), digits = 2), 
+                    "(personalized expenditure factor)",
                     " + 1.39 for basic activities of daily living =", 
-                    round(delta_PAL + 1.39, digits = 2))), 
-            p("Your physical activity category is: ", 
-              strong(gsub("_", " ", PAL_category))))
+                    round(PAL_value(), digits = 2))), 
+            p("Your Physical Activity Level is: ", 
+              span(gsub("_", " ", PAL_category),class="emphasizeText")))
         
     })
-    
 
 })
 
@@ -218,16 +292,17 @@ zoomPlot <- function(data) {
     ageStep <- ageThresh / 20
     bmiThresh <- max(abs(data$bmiDiff))
     bmiStep <- bmiThresh / 20
-    odd <- FALSE
-    while(nrow(data) > 20) {
-        if(odd) {
-            ageThresh <- ageThresh - ageStep
-            odd=FALSE
-        } else {
-            bmiThresh <- bmiThresh - bmiStep
-            odd=TRUE
-        }
-        data <- filter(data, abs(ageDiff) < ageThresh, abs(bmiDiff) < bmiThresh)
+    palThresh <- max(abs(data$palDiff))
+    palStep <- palThresh / 20
+    cycle <- 2
+    while(nrow(data) > 25) {
+        switch(cycle,
+               {ageThresh <- ageThresh - ageStep; cycle <- 2;},
+               {bmiThresh <- bmiThresh - bmiStep; cycle <- 3;},
+               {palThresh <- palThresh - palStep; cycle <- 1})
+
+        data <- filter(data, abs(ageDiff) < ageThresh, abs(bmiDiff) < bmiThresh,
+                       abs(palDiff) < palThresh)
     }
     data
 } 
--- ui.R
+++ ui.R
@@ -8,28 +8,64 @@
 library(shiny); library(rCharts)
 #options(RCHART_LIB = 'polycharts')
 
+## lengthy text objects are defined here rather than in the nested
+## document structure
+
+PAL_help_text <- div(p("A Physical Activity Level above sedentary is 
+determined from the amount of time spent engaged in exercise or activities 
+beyond the basic tasks associated with normal living. Based on a moderate 
+intensity activity (walking at 4 mph), 30 years of age, and average height 
+and weight, at least 30 minutes of weekly activity would be required to reach 
+the Low Active category, 8 hours per week for Active, and 19 hours per week 
+for Very Active."), p("You can calculate your physical activity level based on 
+your age, height, weight, and activities below."), p("Enter the total number 
+of ", strong("hours per week"), " you spend doing the following activities:"))
+
+intro_text <- div(p("This application provides estimates of daily calorie
+requirements for individuals. Because of differences between people, the 
+result is not an exact value but a range of possible energy levels. The 
+range is determined statistically so that, for 95% of people, their true
+individual energy requirement will be between the lower and upper values.
+"),p("To estimate your individual energy needs, enter your sex,
+age, height, weight, and physical activity level below, then click the 
+\"Calculate Energy Needs\" button. If you are unsure of your physical 
+activity level, click the \"Help\" link in that section to use the physical activity level
+calculator"))
+
 shinyUI(fluidPage(
   tags$head(tags$style(HTML("
             .sidebyside label {width: auto; font-size:12px;}
             .sidebyside input {display: inline-block; width: 70px; float: right;}
             .sidebyside .col-sm-4 {min-width: 300px;}
-            .lowerunits {margin-top:25px;}
-            .floaterPanel {position: fixed; top: 75px; z-index:999;
-                            width: 910px; margin: 0 auto;}"))),    
+            .lowerunits {margin-top:28%;}
+            .floaterPanel {position: absolute; top: 40px; z-index:999;
+                            width: 910px; margin: 0 auto;}
+            .emphasizeText { font-weight: bold; font-size: larger;}
+            body {max-width: 1100px; margin: 0 auto;}"))),    
 
-  # Application title
-  titlePanel("Accurate Estimates of Calorie Needs"),
-  p("Introduction and instructions"),
-  # Sidebar with a slider input for number of bins
+  # Application title and introduction
+  titlePanel("Accurate Estimation of Individual Calorie Needs"),
+  intro_text,
+  conditionalPanel("input.PAL_help_visible>0",
+                   div(
+                       div(class="panel-heading",
+                           h3(class="panel-title", "Determine your physical activity level")),
+                       div(class="panel-body", 
+                           PAL_help_text,
+                           uiOutput("activityList"),
+                           uiOutput("PAL_calc"),
+                           actionButton("PAL_calculate", "Accept and Continue")),
+                       class="panel panel-primary floaterPanel")),
+  # Sidebar with inputs for calculation
   sidebarLayout(
     sidebarPanel(
       radioButtons("sex", "Sex", list("Male"="m", "Female"="f")),
-      fluidRow(column(6, numericInput("age", "Age", value = 30, min = 18)),
+      fluidRow(column(6, numericInput("age", "Age", value = 40, min = 18)),
                column(6, p("Years", class="lowerunits"))),
       fluidRow(column(6, conditionalPanel("input.ht_units != 'cm'",
-                                          textInput("height_ftin", "Height", value="5'9.7\"")),
+                                          textInput("height_ftin", "Height", value="5'9\"")),
                       conditionalPanel("input.ht_units == 'cm'",
-                                       numericInput("height_cm", "Height", value = 177))),
+                                       numericInput("height_cm", "Height", value = 175))),
                column(6,div(radioButtons("ht_units",NULL, 
                                          list(`ft\' in\"` = 'ftin', cm = 'cm'),
                                          inline = TRUE),class="lowerunits"))),
@@ -48,28 +84,17 @@ shinyUI(fluidPage(
       conditionalPanel("false", 
                         numericInput("PAL_help_visible", "is the PAL screen showing?", 
                                      value=0)),
-      actionButton("goButton", "Calculate Energy Needs"),
-      conditionalPanel("input.PAL_help_visible>0",
-                       div(
-                           div(class="panel-heading",
-                           h3(class="panel-title", "Determine your physical activity level")),
-                           div(class="panel-body", p("The physical activity levels are defined as..."),
-                           p("To calculate your physical activity level, enter the total 
-                              number of hours your spend", strong("per week"), 
-                             "doing the following activities:"),
-                           
-                           uiOutput("activityList"),
-                           #textOutput("PAL_calc"),
-                           uiOutput("PAL_calc"),
-                           actionButton("PAL_calculate", "Save and Continue")),
-                           class="panel panel-primary floaterPanel"))
+      actionButton("goButton", "Calculate Energy Needs")
+      
     ),
 
-    # Show a plot of the generated distribution
+    # Results section
     mainPanel(
-      h3("Estimated Energy Needs"),
-      textOutput("EER"),
-      showOutput("chart", "polycharts")
+        
+        h3("Your Estimated Energy Needs"),
+        uiOutput("EER"),
+        showOutput("chart", "polycharts"),
+        textOutput("chart_caption")
     )
   )
 ))
